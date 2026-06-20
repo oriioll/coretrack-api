9deb079373c5055b2877aa3e1a874955fb4708e8
oriioll <oriolplazas2@gmail.com>
2026-06-20 15:12:34 +0200

    feat: add new endpoint /muscles

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MuscleController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get('/test', function (Request $request) {
+    return response()->json([
+        "success" => true,
+    ]);
+});
+
+Route::get('/muscles', [MuscleController::class, 'index']);
